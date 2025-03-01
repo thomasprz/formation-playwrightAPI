d37eb4cf80446027057bd4ce863aea1de55462c4
Thomas <przegralek.thomas@gmail.com>
2025-03-01 21:02:14 +0100

    Ajout du script d'authentification auth.setup.ts

---
+++ tests/auth.setup.ts
@@ -0,0 +1,25 @@
+import {test as setup} from '@playwright/test'
+import user from '../.auth/user.json'
+import fs from 'fs'
+
+const authFile = '.auth/user.json'
+
+setup('authentification', async ({page, request}) => {
+
+const response = await request.post('https://conduit-api.bondaracademy.com/api/users/login', {
+        data: {
+            "user": {
+                "email": "tom.pcheg@gmail.com",
+                "password": "Lille-59000"
+            }
+        }
+    })
+const responseBody = await response.json();
+const accessToken = responseBody.user.token
+console.log(responseBody)
+
+user.origins[0].localStorage[0].value = accessToken
+fs.writeFileSync(authFile, JSON.stringify(user))
+
+process.env['ACCESS_TOKEN'] = accessToken
+})
